3f6b03463421922045cee886efe92fd7be6e69e7
Timmy Weerwag <timmy@timmyweerwag.nl>
2012-08-22 18:47:42 +0200

    Initial commit, with directory structure

---
+++ README.txt
@@ -0,0 +1,6 @@
+=======
+Clieopy
+=======
+
+Clieopy is a module for accessing CLIEOP03 files. This format is used by Dutch
+bankers for batch payments.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Clieopy',
+    version='0.0.1',
+    author='Timmy Weerwag',
+    author_email='timmy@timmyweerwag.nl',
+    packages=['clieopy'],
+    license='LICENSE.txt',
+    description='Access (reading/writing) to CLIEOP03 files.',
+    long_description=open('README.txt').read(),
+)
